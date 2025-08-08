6a27300bafb59a782d1c19d3f573ab20f85908a1
mjlee76 <dlalswns0422@gmail.com>
2025-08-08 18:14:35 +0900

    25.08.08

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hello_spring;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration // 직접 스프링 컨테이너에 Bean 등록하는 방법
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository(); // 인터페이스(MemberRepository)는 new가 안됨
+        //return new MemoryMemberRepository(); // 인터페이스(MemberRepository)는 new가 안됨
                                              // -> 구현체(MemoryMemberRepository)를 return해줌
+                                                // 메모리에만 데이터를 저장하던 임시 구현체
+        return new JdbcMemberRepository(dataSource);      // 데이터베이스(H2 등)에 JDBC로 연결하여 실제 영속 저장하도록 변경
     }
 }
--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller  // Spring 컨테이너에 컨트롤러가 담기게 됨
 public class MemberController {
@@ -12,5 +18,30 @@ public class MemberController {
     @Autowired  // 생성자 주입: Spring 컨테이너에 있는 memberService를 가져와서 자동으로 연결해줌
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // 홈화면으로 보내기
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMemebers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
